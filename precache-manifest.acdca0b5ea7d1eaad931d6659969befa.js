81d04795e9aee42f9604b81dcd7dcb0eefb4bc45
Nobert Momoh <okhai0112@gmail.com>
2020-02-21 02:30:13 +0100

    Updates

---
+++ precache-manifest.acdca0b5ea7d1eaad931d6659969befa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a036f4b1737c0d3aa7645417b7209e48",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "a4dd1634eaff2366403f",
+    "url": "/react-monsters/static/css/main.e9641147.chunk.css"
+  },
+  {
+    "revision": "9a603ea88eb87a3af172",
+    "url": "/react-monsters/static/js/2.a2e47249.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-monsters/static/js/2.a2e47249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4dd1634eaff2366403f",
+    "url": "/react-monsters/static/js/main.c68df3b3.chunk.js"
+  },
+  {
+    "revision": "3ba78d37e13dfa20bba5",
+    "url": "/react-monsters/static/js/runtime-main.cb69df4f.js"
+  }
+]);
